9c69141e08c185adb68742a6877a005becd139b2
Rique <145708015+ririque@users.noreply.github.com>
2024-05-13 15:30:56 -0300

    New dashboard

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,11 @@
 local M = {
-  "LunarVim/darkplus.nvim",
+  "catppuccin/nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  name = "catppucin",
   priority = 1000, -- make sure to load this before all the other start plugins
 }
-
 function M.config()
-  vim.cmd.colorscheme "darkplus"
+  vim.cmd.colorscheme "catppuccin-mocha"
 end
 
 return M
