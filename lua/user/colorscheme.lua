b12012edede1d9c7cb59ad2f39f87511a6edfdfd
Rique <145708015+ririque@users.noreply.github.com>
2024-05-31 14:29:19 -0300

    New color

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,11 @@
 local M = {
-  "diegoulloao/neofusion.nvim",
+  "catppuccin/nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = true,
 }
 function M.config()
-  vim.cmd.colorscheme "neofusion"
+  vim.cmd.colorscheme "catppuccin"
 end
 
 return M
